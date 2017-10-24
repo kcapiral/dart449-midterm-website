830b33fe4bf49226db5c7a74fbf3f109259af042
kcapiral <pie_and_cream2@hotmail.com>
2017-10-23 20:19:17 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('#first-fadein').hide().delay(250).fadeIn(250);
+	$('#second-fadein').hide().delay(500).fadeIn(250);
+	$('#third-fadein').hide().delay(750).fadeIn(250);
+	$('#fourth-fadein').hide().delay(1000).fadeIn(250);
+});
